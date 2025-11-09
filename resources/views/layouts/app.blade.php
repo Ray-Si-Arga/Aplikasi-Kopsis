852da03744f790bbbbc5020fd6544abf1d1dee42
Qwaasong <aldiker700@gmail.com>
2025-11-09 10:17:49 +0700

    Memperbaiki Path Jquery

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,11 +1,14 @@
 <!DOCTYPE html>
 <html>
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>@yield('title')</title>
     @vite(['resources/css/app.css', 'resources/js/app.js'])
     <link rel="stylesheet" href="{{ asset('assets/css/app.css') }}">
+    <script src="{{ asset('assets/js/jquery-3.7.1.min.js') }}"></script>
 </head>
 
 <body>
@@ -378,7 +381,7 @@
             </div>
 
             <!-- Main Content Area -->
-            <div class="flex-1 flex flex-col overflow-hidden">
+            <div class="flex-1 flex flex-col overflow-y-auto">
                 @yield('content')
             </div>
         </div>
@@ -386,4 +389,5 @@
     <script src="{{ asset('assets/js/app.js') }}"></script>
     @yield('script')
 </body>
+
 </html>
